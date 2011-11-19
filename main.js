281f823fe08c23aa4462985d9ef3abf349107d22
Patrick Pfeiffer <patrick@buzzle.at>
2011-11-19 18:20:15 +0100

    The "sys" module is now called "util"

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-var sys = require('sys')
+var sys = require('util')
   , fs = require('fs')
   , path = require('path')
   , events = require('events')
